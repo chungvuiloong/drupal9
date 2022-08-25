2da83545911f1172fdb6b12ae42bb6c17bfef86e
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-08-25 13:30:27 +0300

    2022/08/25

---
+++ modules/custom/first_module/src/Controller/FirstController.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace Drupal\first_module\Controller;
+
+class FirstController {
+    public function first () {
+        return array('#markup' => "Welcome to MrJays Drupal Website");
+    }
+}
